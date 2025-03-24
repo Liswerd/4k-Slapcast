ad1f96b976dd8098ea3aefe1d71aa49777f3410c
Nateguana <nateguana@gmail.com>
2025-03-24 10:59:24 -0400

    Add project files.

---
+++ Source/Slapcast/Slapcast.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Slapcast.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Slapcast, "Slapcast" );
+ 
---
+++ Source/Slapcast/SlapcastGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlapcastGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASlapcastGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASlapcastGameMode();
+};
+
+
+
